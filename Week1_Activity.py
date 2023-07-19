86e1da8431be79213a9b07ade187c2fbcb609182
rhempenstall <u1080409@umail.usq.edu.au>
2023-07-19 20:44:14 +1000

    Step 2 Commit

---
+++ Week1_Activity.py
@@ -0,0 +1,13 @@
+# write code in python that prints your name
+
+name = "Rebecca"
+
+print(name)
+
+# and then counts up your favourite number squared
+
+favourite_number = 3
+
+print(favourite_number * favourite_number)
+
+# commit this code to your own GitHub
